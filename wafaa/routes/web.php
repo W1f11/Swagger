42be4825a592e21ad23ebf829cfecaf697528d29
Wafaa <essalhiwafaa05@gmail.com>
2025-10-22 09:27:02 +0100

    1

--- wafaa/routes/web.php
+++ wafaa/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Duplicate artist routes for non-/api access (serve JSON)
+use App\Http\Controllers\ArtistController;
+
+Route::middleware('api')->group(function () {
+    Route::get('artists', [ArtistController::class, 'index']);
+    Route::post('artists', [ArtistController::class, 'store']);
+    Route::get('artists/{artist}', [ArtistController::class, 'show']);
+    Route::put('artists/{artist}', [ArtistController::class, 'update']);
+    Route::patch('artists/{artist}', [ArtistController::class, 'update']);
+    Route::delete('artists/{artist}', [ArtistController::class, 'destroy']);
+});
